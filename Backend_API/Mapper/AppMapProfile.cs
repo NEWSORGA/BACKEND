c2f96a67ed5cfc366d0694c5c4178d8b57f93d03
Uamixerz <Uamixerz@gmail.com>
2023-08-16 20:31:23 +0300

    settings for comments 1.0

--- Backend_API/Mapper/AppMapProfile.cs
+++ Backend_API/Mapper/AppMapProfile.cs
@@ -1,21 +1,32 @@
-﻿using ASP_API.Models;
+﻿using ASP_API.Helpers;
+using ASP_API.Models;
 using ASP_API.Models.Comments;
 using ASP_API.Models.LikeTweet;
 using AutoMapper;
+using Backend_API.Data;
 using Backend_API.Data.Entities;
 using Backend_API.Data.Entities.Identity;
 using Backend_API.Models.Auth;
+using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp;
 
 namespace ASP_API.Mapper
 {
     public class AppMapProfile : Profile
     {
+        
+
         public AppMapProfile()
         {
             CreateMap<TweetMediaEnitity, TweetViewImageModel>();
             CreateMap<LikeTweetViewModel, TweetLikeEntity>();
             CreateMap<CommentMediaEntity, CommentsViewImageModel>();
             CreateMap<UserEntity, UserViewModel>().ForMember(s => s.IsFollowed, opt => opt.MapFrom(s => false));
+            CreateMap<CommentEntity, CommentsGetViewModel>().ForMember(s => s.CreatedAtStr, opt => opt.MapFrom(s => HelperFunctions.ConvertDateTimeToStr(s.CreatedAt)))
+                .ForMember(dest => dest.CommentsChild, opt => opt.MapFrom(src => src.CommentsChildren));
+            CreateMap<CommentMediaEntity, CommentsViewImageModel>();
         }
+
+        
     }
 }
--- Backend_API/Models/Comments/CommentsViewModel.cs
+++ Backend_API/Models/Comments/CommentsViewModel.cs
@@ -10,6 +10,20 @@
 
 
     }
+
+    public class CommentsGetViewModel
+    {
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+        public int TweetId { get; set; }
+        public List<CommentsViewImageModel>? Images { get; set; }
+        public int UserId { get; set; }
+        public int? CommentParentId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedAtStr { get; set; }
+        public List<CommentsGetViewModel>? CommentsChild { get; set; }
+
+    }
     public class CommentsUploadImageModel
     {
         public IFormFile Media { get; set; }
